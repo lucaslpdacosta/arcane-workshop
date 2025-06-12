60c2b26bfb87ccf3e3704441dfadae64760c6e1c
Lucas Prudencio <lucaslpdacosta1@gmail.com>
2025-06-12 03:44:56 -0300

    feat: implemented tasks list on page

--- oraculo/views.py
+++ oraculo/views.py
@@ -3,12 +3,14 @@ from rolepermissions.checkers import has_permission
 from django.http import Http404
 from django.views.decorators.csrf import csrf_exempt
 from .models import Treinamentos
+from django_q.models import Task
 
 def treinar_ia(request):
     if not has_permission(request.user, 'treinar_ia'):
         raise Http404()
     if request.method == 'GET':
-        return render(request, 'treinar_ia.html')
+        tasks = Task.objects.all()
+        return render(request, 'treinar_ia.html', {'tasks': tasks})
     elif request.method == 'POST':
         site = request.POST.get('site')
         conteudo = request.POST.get('conteudo')
